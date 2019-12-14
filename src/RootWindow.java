6d821874fcd7366bf37a20c2511d68c9d8068c91
ifritee <ifritee@gmail.com>
2019-12-14 10:10:49 +0300

    Добавил файлы проекта, 0.1a

---
+++ src/RootWindow.java
@@ -0,0 +1,134 @@
+import sun.misc.CRC16;
+import sun.plugin2.gluegen.runtime.CPU;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.CRC32;
+
+/**
+ * Основное окно для ПО "Подмена J"
+ */
+public class RootWindow {
+    private JPanel rootPanel;
+    private JComboBox cb_crc_type;
+    private JComboBox cb_file_type;
+    private JTextField tf_set_file;
+    private JButton btn_choose_file;
+    private JLabel lb_crc_dec;
+    private JTextField tf_crc_dec;
+    private JTextField tf_crc_hex;
+    private JTextField tf_crc_oct;
+    private JButton btn_calculate;
+    private JButton btn_cancel;
+    private JLabel lb_crc_hex;
+    private JLabel lb_crc_oct;
+    /** Выбранный файл */
+    private File _selectedFile;
+    /** Конструктор */
+    public RootWindow() {
+        _selectedFile = new File("");
+        btn_choose_file.addActionListener(e -> { // Действия при выборе файла
+            JFileChooser fileChooser = new JFileChooser();
+            int ReturnCode_i = fileChooser.showOpenDialog(rootPanel);
+            if (ReturnCode_i == JFileChooser.APPROVE_OPTION) {
+                _selectedFile = fileChooser.getSelectedFile();
+                tf_set_file.setText(_selectedFile.getName());
+                btn_calculate.setEnabled(true);
+                if(cb_crc_type.getSelectedItem() == "CRC16") {
+                    CRC16Calculate_v();
+                } else if (cb_crc_type.getSelectedItem() == "CRC32") {
+                    CRC32Calculate_v();
+                } else if (cb_crc_type.getSelectedItem() == "CKSum") {
+                    CKSumCalculate_v();
+                }
+            } else { // Ошибка открытия
+                btn_calculate.setEnabled(false);
+            }
+        });
+        cb_crc_type.addActionListener(e -> { // Действия при выборе типа КС
+            if (_selectedFile.exists() && _selectedFile.canRead()) {
+                if(cb_crc_type.getSelectedItem() == "CRC16") {
+                    CRC16Calculate_v();
+                } else if (cb_crc_type.getSelectedItem() == "CRC32") {
+                    CRC32Calculate_v();
+                } else if (cb_crc_type.getSelectedItem() == "CKSum") {
+                    CKSumCalculate_v();
+                }
+            }
+        });
+        btn_calculate.addActionListener(e -> {  // Действия при расчете
+            int CPUCount_i = Runtime.getRuntime().availableProcessors();
+            System.out.println(CPUCount_i);
+        });
+    }
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+
+    /** Подсчет КС по алгоритму CRC32 */
+    private void CRC32Calculate_v() {
+        try {
+            CRC32 CRC32_o = new CRC32();
+            FileInputStream fis = new FileInputStream(_selectedFile);
+            int c;
+            while ((c = fis.read()) != -1) {
+                CRC32_o.update(c);
+            }
+            lb_crc_dec.setText("DEC: " + String.valueOf(CRC32_o.getValue()));
+            lb_crc_hex.setText("HEX: " + Long.toHexString(CRC32_o.getValue()).toUpperCase());
+            lb_crc_oct.setText("OCT: " + Long.toOctalString(CRC32_o.getValue()));
+        } catch (IOException e) {
+
+        } catch (NullPointerException e) {
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        }
+    }
+
+    /** Подсчет КС по алгоритму CRC32 */
+    private void CRC16Calculate_v() {
+        try {
+            CRC16 CRC32_o = new CRC16();
+            FileInputStream fis = new FileInputStream(_selectedFile);
+            int c;
+            while ((c = fis.read()) != -1) {
+                CRC32_o.update((byte) c);
+            }
+            lb_crc_dec.setText("DEC: " + String.valueOf(CRC32_o.value));
+            lb_crc_hex.setText("HEX: " + Long.toHexString(CRC32_o.value).toUpperCase());
+            lb_crc_oct.setText("OCT: " + Long.toOctalString(CRC32_o.value));
+        } catch (IOException e) {
+
+        } catch (NullPointerException e) {
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        }
+    }
+
+    /** Подсчет КС по алгоритму CKSum */
+    private void CKSumCalculate_v() {
+        try {
+            JCKSum JCKSum_o = new JCKSum();
+            FileInputStream fis = new FileInputStream(_selectedFile);
+            int c;
+            while ((c = fis.read()) != -1) {
+                JCKSum_o.addByte_v(c);
+            }
+            int Value_i = JCKSum_o.getValue_i();
+            lb_crc_dec.setText("DEC: " + Value_i);
+            lb_crc_hex.setText("HEX: " + Integer.toHexString(Value_i).toUpperCase());
+            lb_crc_oct.setText("OCT: " + Integer.toOctalString(Value_i));
+        } catch (IOException e) {
+
+        } catch (NullPointerException e) {
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        }
+    }
+}
